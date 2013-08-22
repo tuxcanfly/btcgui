d2259e0ff42080a6c12ee96a3e8445cf759279f5
Josh Rickmar <jrick@conformal.com>
2013-08-22 12:07:15 -0400

    Enter on timeout activates dialog response.

--- unlockdialog.go
+++ unlockdialog.go
@@ -89,6 +89,9 @@ func createUnlockDialog() *gtk.Dialog {
 		log.Fatal(err)
 	}
 	timeout.SetValue(60)
+	timeout.Connect("activate", func () {
+		dialog.Emit("response", gtk.RESPONSE_OK, nil)
+	})
 	grid.Attach(timeout, 1, 2, 1, 1)
 
 	dialog.SetTransientFor(mainWindow)
